91847344a5ee16a3553b447f947222275998892f
AUTOSAFE <107992265+AUTOSAFE@users.noreply.github.com>
2022-09-01 22:36:47 +0800

    Update lana.sh
    Signed-off-by: AUTOSAFE <107992265+AUTOSAFE@users.noreply.github.com>

--- lana.sh
+++ lana.sh
@@ -1,113 +1,107 @@
-+
+#!/usr/bin/bash
+
+#warna
+default="\033[00m"
+merah="\033[31m"
+hijau="\033[32m"
+kuning="\033[33m"
+biru="\033[34m"
+birumuda="\033[35m"
+ungu="\033[36m"
+putih="\033[37m"
 
 
-uں[t}4
-ڇMY
-(ڻMi
+clear
+echo
+   echo -e ""  $merah "██╗░░░██╗██╗██████╗░" $merah "██╗░░░░░░█████╗░███╗░░██╗░█████╗"
+   echo -e ""  $merah "██║░░░██║██║██╔══██╗" $merah "██║░░░░░██╔══██╗████╗░██║██╔══██╗"
+   echo -e ""  $merah "╚██╗░██╔╝██║██████╔╝" $merah "██║░░░░░███████║██╔██╗██║███████║"
+   echo -e ""  $putih "░╚████╔╝░██║██╔═══╝░" $putih "██║░░░░░██╔══██║██║╚████║██╔══██║"
+   echo -e ""  $putih "░░╚██╔╝░░██║██║░░░░░" $putih "██║░░░░░██╔══██║██║╚████║██╔══██║"
+   echo -e ""  $putih "░░░╚═╝░░░╚═╝╚═╝░░░░░" $putih "╚══════╝╚═╝░░╚═╝╚═╝░░╚══╝╚═╝░░╚═╝" 
+ 
+   echo
+   echo -e $kuning" ╔═══════════════════════════════════════════════╗"
+   echo -e $kuning" ║"   "\033[34mAuther    : Lana                          "$kuning"    ║"
+   echo -e $kuning" ║"   "\033[34mTeam      : Zexxy                         "$kuning"    ║"
+   echo -e $kuning" ║"   "\033[32mWA : 088245059338                         "$hijau"    ║"
+   echo -e $kuning" ║"   "\033[34mGithub    : https://github.com/AUTOSAFE   "$kuning"    ║"
+   echo -e $kuning" ╚═══════════════════════════════════════════════╝"
 
-7}
-n*}
-n*Z}
-x.}
-bM߹
+   echo -e $putih
+   echo -e $putih "╔════════════════════════╗"
+   echo -e " ║ " "\033[1;31m Script BY lana" $putih "     ║"
+   echo -e $putih "╚════════════════════════╝"
 
+echo -e  $putih"Selamat Datang Perkumpulan script"
 
-rW
-yh
-yh鞭fz
-yh鞭fz
-yh鞭fz
-yhn(iء
-yhn(iء
-yhn(iء
-
-yh
-yh.)
-yh.)}`.-ڒ
-yh.)}djf^x
-yh.)}e69}(
-yh.)}ainm&9 Kx
-yh.)
-
-yhn(
-yhn(
-yh7]'+AbVj
-yhn(
-
-yhn(RzVjڵ=jnr
-
-yhbzW(䢕djs(
-yhRr'
-yh맂r+n^
-0jv
-yhrn^
-0
-0jv
-yhbԭfy:Ы	K${
-yh鞭yԭfx*pr-z
-yhr+n^
-0N) r-
-yh맂r*Z)"
-yhbIԭfxV*pr-
-yhuD+
+echo -e  $hijau"Welcome Silahkan Pilih"
+echo -e  $biru"[1] Script Bussid"
+echo -e  $ungu"[2] Script Stumble Crown By:Lana"
+echo -e  $biru"[3] Script stumble Crown and Crown By:Lana"
+echo -e  $hijau"[4] script Stumble Trophy and Crown By:Eskey"
+echo -e  $merah"[5] script Stumble Crown By:DKimposter"
+echo -e  $biru"[6] script Stumble Crown and Trophy By:Lana"
+echo -e  $ungu"[7] Script Spam Sms By:Khfz"
+echo -e  $hijau"[8] Script Stumble Fast Crown By:Lana"
+echo -e  $birumuda"[9] Exit"
 
 
-yh罹)bv
-b[az
-+\ކinm&!KϜj~n,ze
-qϜj~n,
-*bZZު笶
-+avȝ
-yhIZCj֧ؒr'
-zX)v
-+\ކinm&9 )joze
-q)j
-
-
-yhZ]j֧جrV
-zX)w
-+\ކinm&9 
-q
-
-^w^
-yhZ]j֧جrV
-zX)x
-H.
-ud]j
-H"ZX" {-jY[j
-+\ކinm&K${,J䜮*m
-qԡzY~I
-m!
-yhZ]j֧جr${
-zX)y
-+\ކinm&&-Ҷ雕ޖ
-qԭfxw
-Z
-^w^;
-yhZ]j֧جr|
-zX)z
-+\ކinm&9 }
-qԚ}
-
-^w^
-yhZ]j֧جr
-zX){
-+\ކinm&ҥ
-qԩjk
-*bZZު笶
-*bZW(ڙ
-+ar:r
-yhZ]j֧ؒrjؚ
-zX)|
-jn֭ym+i׾
-H"ZX"
-+\ކinm&9 b
-H"ZZa
-qb
-fj)
-yhIZ]j֧8r$0	
-zX)}
-{
-z[
-yhznƥj
-~
+echo -e -n "pilih > ";read pil
+if [ $pil = "1" ];then
+  git clone https://github.com/KIPASGTS/Mass-cash-bussid &> /dev//null
+  cd Mass-cash-bussid
+  pip install requests
+  python bussid.py
+  echo "Selamat Datang di Script Bussid"
+elif [ $pil = "2" ];then
+  git clone https://github.com/AUTOSAFE/spam &> /dev//null
+  cd spam
+  npm i
+  npm start
+echo "selamat datang di script lana"
+elif [ $pil = "3" ];then
+  git clone https://github.com/AUTOSAFE/bil
+  cd bil
+  npm i
+  node index
+echo "selamat datang di script lana"
+elif [ $pil = "4" ];then
+  pkg upgrade && pkg update
+  pkg install git && pkg install bash
+  git clone https://github.com/Eskeyz/Shell-Scripts.git
+  cd Shell-Scripts
+  bash main.sh
+echo "selamat datang di script eskey"
+elif [ $pil = "5" ];then
+  git clone https://github.com/dkmpostor/StumbleHelper
+  cd StumbleHelper
+  npm install
+  node index.js
+echo "selamat datang di script nyasar"
+elif [ $pil = "6" ];then
+  git clone https://github.com/AUTOSAFE/Safe2
+  cd Safe2
+  npm i
+  node index
+echo "selamat datang di script Lana"
+elif [ $pil = "7" ];then
+  git clone https://github.com/K4HFZ/Spams
+  cd Spams
+  pip install requests
+  pip install colorama
+  python Khfz.py
+echo "selamat datang di Script Kematian"
+elif [ $pil = "8" ];then
+  apt update && apt upgrade -y
+  pkg install git
+  git clone https://github.com/AUTOSAFE/hi
+  pkg install php
+  cd hi
+  php main.php
+echo "Selamat datang Di Script STUMBLE CROWN"
+elif [ $pil = "9" ];then
+exit
+else
+echo "input salah"
+fi
